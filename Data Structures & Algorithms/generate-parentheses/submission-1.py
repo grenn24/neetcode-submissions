294bf801aeebdc4115611663b1d741b3ae501382
Hoo Di Heng <126308058+grenn24@users.noreply.github.com>
2026-08-11 00:14:01 +0800

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        self.path = ""
+        self.leftOpen = 0
+        self.rightOpen = 0
+
+        def helper():
+            if len(self.path) == n * 2:
+                result.append(self.path)
+
+            
+            if self.leftOpen >= self.rightOpen and self.leftOpen < n:
+                self.path += "("
+                self.leftOpen += 1
+
+                helper()
+
+                self.path = self.path[:-1]
+                self.leftOpen -= 1
+            
+            if self.leftOpen > self.rightOpen and self.rightOpen < n:
+                self.path += ")"
+                self.rightOpen += 1
+
+                helper()
+
+                self.path = self.path[:-1]
+                self.rightOpen -= 1
+
+            return
+
+        
+        helper()
+
+        return result
